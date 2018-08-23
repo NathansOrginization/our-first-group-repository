058b3e304ff5b674f7fb4da8e7d78dd5f7e0a2ed
bxkrueper <benkrueper@gmail.com>
2018-08-22 17:16:32 -0700

    greetings

---
+++ Greetings.java
@@ -0,0 +1,20 @@
+/**
+ * @(#)Greetings.java
+ *
+ *
+ * @author 
+ * @version 1.00 2014/6/27
+ */
+
+
+public class Greetings {
+
+    public static void main(String[] args) {
+    	String firstName = args[0];
+    	String lastName = args[1];
+    	System.out.println("Hello, " + firstName + " " + lastName);
+    	System.out.println("Congratulations on your second program!");
+    }
+    
+    
+}
